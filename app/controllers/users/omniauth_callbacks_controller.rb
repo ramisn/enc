2a692b0d80914b2457cc9068dc6f2d0ff18fb0fa
Ramesh K <rameshkror@gmail.com>
2014-01-25 08:26:23 +0530

    session

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
+    @user=User.first
     #@user = User.find_for_facebook_oauth(request.env["omniauth.auth"].provider, request.env["omniauth.auth"].uid, request.env["omniauth.auth"].extra.raw_info.name, request.env["omniauth.auth"].info.email, current_user)
  
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
+      redirect_to root_url
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 Encorenation::Application.config.session_store :cookie_store, key: '_encorenation_session'
+
+require 'action_dispatch/middleware/session/dalli_store'
+if Rails.env == "production" 
+  Rails.application.config.session_store :dalli_store#, :memcache_server => MemCacheServers #, :namespace => 'sessions', :key => '_foundation_session', :expire_after => 30.minutess
+else
+  Rails.application.config.session_store :dalli_store        
+end 
